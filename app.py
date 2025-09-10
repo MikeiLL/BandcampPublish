37453396cfc79c4a8878a7da08b91c1da69d7773
mikeill <mike@mzoo.org>
2025-09-10 10:10:15 -0500

    Use re.search to check for bandcamp url, display some details as well as message if previously published.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import re
 import sys
 import requests
 import subprocess
@@ -6,12 +7,19 @@ import config
 from html.parser import HTMLParser
 from base64 import b64encode
 
-proc = subprocess.run(["pbpaste"], capture_output=True)
+proc = subprocess.run(["pbpaste"], capture_output=True, encoding="UTF-8")
 trackurl = proc.stdout
-#trackurl = "https://chrisbutler1.bandcamp.com/track/impeachment-day"
+
+if not re.match("https://.*bandcamp.*[track|album]", trackurl):
+  print(f"""Hey! You must copy a bandcamp track or album link
+        eg: https://chrisbutler1.bandcamp.com/track/impeachment-day
+        You gave us {trackurl[:30]}
+""")
+  sys.exit()
 
 ITEM_ID = 0
 ITEM_TYPE = "t"
+ITEM_TITLE = ""
 
 class ParseBandcamp(HTMLParser):
 
@@ -22,7 +30,8 @@ class ParseBandcamp(HTMLParser):
         content = json.loads(attrs.get("content"))
         global ITEM_ID; ITEM_ID = content.get("item_id")
         global ITEM_TYPE; ITEM_TYPE = content.get("item_type")
-
+      if attrs.get("name") == "title":
+        global ITEM_TITLE; ITEM_TITLE = attrs["content"]
 pagerequest = requests.get(trackurl)
 
 ParseBandcamp().feed(pagerequest.content.decode())
@@ -30,19 +39,32 @@ ParseBandcamp().feed(pagerequest.content.decode())
 if not ITEM_ID:
   print("Something went wrong. No Item ID.")
   sys.exit()
-resp = requests.put(F"https://api.github.com/repos/MikeiLL/Playground/contents/{ITEM_ID}",
+
+print(f"""Okay great! We got the data from Bandcamp.
+Looks like the {"track" if ITEM_TYPE == 't' else 'album'},  {ITEM_TITLE}.
+Publishing the player now...
+""")
+
+content = {
+  "type": ITEM_TYPE,
+  "title": ITEM_TITLE,
+}
+resp = requests.put(F"https://api.github.com/repos/MikeiLL/futurefossilmusicsite/contents/_data/bandcamptracks/{ITEM_ID}.json",
     json={
         "message":f"add {"track" if ITEM_TYPE == 't' else "Album"}: {ITEM_ID}",
         "committer":{
           "name":"BC Publish Rosuav and Mike",
               "email":"mike@mzoo.org"
             },
-        "content": b64encode(b"{'type': %s}" % ITEM_TYPE.encode()).decode()
+        "content": b64encode(json.dumps(content).encode()).decode(),
     },
       headers={
   "Accept": "application/vnd.github.object",
   "Authorization": "Bearer " + config.githubtoken,
   "X-GitHub-Api-Version": "2022-11-28",
 })
+if resp.status_code == 422:
+  print("Hmmm. Is that track already posted?\n")
+  sys.exit()
 resp.raise_for_status()
-print(resp.content)
+print("Success. The new bandcamp player is publishing this very moment. It could take up to 15 minutes.")
